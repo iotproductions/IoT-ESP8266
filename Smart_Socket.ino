3d07014de0d306153454652ff0f9d5189fe9020a
Le Thanh Trieu <lethanhtrieuk36@gmail.com>
2017-03-18 15:06:01 +0700

    Create Smart_Socket.ino

---
+++ Smart_Socket.ino
@@ -0,0 +1,93 @@
+/*
+  This a simple example of the aREST Library for the ESP8266 WiFi chip.
+  See the README file for more details.
+
+  Written in 2015 by Marco Schwartz under a GPL license.
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <aREST.h>
+
+#define DEBUG_MODE 1
+// Create aREST instance
+aREST rest = aREST();
+
+// WiFi parameters
+const char* ssid = "BASEMENT01";
+const char* password = "rynan@2020";
+
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Variables to be exposed to the API
+int temperature;
+int humidity;
+
+// Declare functions to be exposed to the API
+int ledControl(String command);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+  pinMode(4,OUTPUT);
+  // Init variables and expose them to REST API
+  temperature = 24;
+  humidity = 40;
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+
+  // Function to be exposed
+  rest.function("led",ledControl);
+
+  // Give name & ID to the device (ID should be 6 characters long)
+  rest.set_id("1");
+  rest.set_name("esp8266");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  // Handle REST calls
+  WiFiClient client = server.available();
+    
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+     Serial.println("Delay");
+    delay(1);
+  }
+  rest.handle(client);
+
+}
+
+// Custom function accessible by the API
+int ledControl(String command) {
+
+  // Get state from command
+  int state = command.toInt();
+  Serial.println("Received: ");
+  Serial.println(command);
+  digitalWrite(2,state);
+  return 1;
+}
