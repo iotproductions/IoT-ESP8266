c903d6522152fea73ead4d9acc2e9c114df90be0
Le Thanh Trieu <lethanhtrieuk36@gmail.com>
2017-04-16 10:00:29 +0700

    Update mqtt_nodejs.js

--- MQTT/mqtt_nodejs.js
+++ MQTT/mqtt_nodejs.js
@@ -1,10 +1,13 @@
-var mqtt = require('mqtt'); 
-
+//16-Apri-2017: Create new project
+// @reboot sudo node /home/pi/nodejs/mqtt.js
+var mqtt = require('mqtt')
+var schedule = require('node-schedule');
+ 
 var client = mqtt.connect(
 	{ 	
-		host: 'localhost', 
-		port: 1883, 
-		keepalive: 10000,
+		host: '*****************', 
+		port: 1873, 
+		keepalive: 60000,
 		username: '**********',
 		password: '**********',
 		protocolId: 'MQIsdp',
@@ -12,13 +15,38 @@ var client = mqtt.connect(
 	});
 
 client.on('connect', function(){
-    console.log('Connected to Broker');
+    console.log('Connected to Broker');	
+	var j = schedule.scheduleJob(' */5 * * * *', function(){
+	  var publish_options = {
+		  retain:true,
+		  qos: 1
+	  };	
+	  client.publish('TOPIC_NAME', '\r\nDATA\r\n',publish_options);
+	  console.log('Publish message to Broker');
+	});
 });
+// Subcribe all topic
 client.subscribe('#');
  
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(topic.toString());
   console.log(message.toString());
-  client.end();
+  //client.end();
 });
+
+client.on('close', function () {
+  // Reconnect to Broker
+  client = mqtt.connect(
+	{ 	
+		host: '*****************', 
+		port: 1873, 
+		keepalive: 60000,
+		username: '**********',
+		password: '**********',
+		protocolId: 'MQIsdp',
+		protocolVersion: 3
+	});
+});
+
+
